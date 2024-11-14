01dd1459f3fbccfbf4acf77a524443cb1f6916dd
Henrique Oliveira <henriaug12@gmail.com>
2024-11-14 16:03:02 -0300

    Funcionalidade: edição do usuário pelo admin

--- codigo/assets/js/telaAdmin.js
+++ codigo/assets/js/telaAdmin.js
@@ -17,7 +17,7 @@ window.addEventListener("load", async () => {
     }
     const response = await fetch("http://localhost:3000/clientes/", options);
     const vetorClientes = await response.json();
-    for(let i = 0; i < vetorClientes.length; i++){
+    for(let i = 0; i < vetorClientes.length && i < 9; i++){
         let isAdmin = "Não";
         if(vetorClientes[i].admin == true) {
             isAdmin = "Sim";
@@ -28,12 +28,15 @@ window.addEventListener("load", async () => {
             <td>${vetorClientes[i].email}</td>
             <td>${isAdmin}</td>
             <td>
-                <button class="btnEditar">Editar</button>
+                <button class="btnEditarUsuario">Editar</button>
                 <button class="btnDeletar">Deletar</button>
             </td>
         </tr>`;
     }
-    let btnsEditar = document.querySelectorAll(".btnEditar");
+    if(vetorClientes.length >= 9){
+        // paginação
+    }
+    let btnsEditar = document.querySelectorAll(".btnEditarUsuario");
     btnsEditar.forEach(btn => {
         btn.addEventListener('click', () => {
             editarUsuario(btn);
@@ -48,6 +51,36 @@ window.addEventListener("load", async () => {
 })
 async function editarUsuario(btn){
     let idUsuario = btn.parentElement.parentElement.id;
+    let telaPadrao = document.getElementById("telaPadrao");
+    let modalUsuario = document.getElementById("modalUsuario");
+    let btnVoltar = document.getElementById("btnVoltar");
+    telaPadrao.style.display = "none";
+    modalUsuario.style.display = "block";
+    let inputNome = document.getElementById("inputNome");
+    let inputEmail = document.getElementById("inputEmail");
+    let inputSenha = document.getElementById("inputSenha");
+    let inputConfSenha = document.getElementById("inputConfSenha");
+
+    let botaoEditarNome = document.getElementById("botaoNome");
+    botaoEditarNome.addEventListener("click", ()=>{
+        editarNome(inputNome, botaoEditarNome, idUsuario)
+    });
+    let botaoEditarEmail = document.getElementById("botaoEmail");
+    botaoEditarEmail.addEventListener("click", ()=>{
+        editarEmail(inputEmail, botaoEditarEmail, idUsuario)
+    });
+    let botaoSenha = document.getElementById("botaoSenha");
+    botaoSenha.addEventListener("click", ()=>{
+        editarSenha(inputSenha, inputConfSenha, botaoSenha, idUsuario)
+    });
+
+    receberInformacoes(idUsuario);
+
+    btnVoltar.addEventListener("click",()=>{
+        modalUsuario.style.display = "none";
+        telaPadrao.style.display = "block";
+        location.reload();
+    })
 }
 
 async function deletarUsuario(btn){
@@ -70,5 +103,145 @@ async function deletarUsuario(btn){
             console.log(err);
             alert("Houve um erro");
         }
+    }
+}
+async function editarNome(inputNome, botaoEditarNome, idUsuario) {
+    if (inputNome.hasAttribute('readonly')) {
+        inputNome.removeAttribute('readonly');
+        botaoEditarNome.innerHTML = "Salvar";
+        botaoEditarNome.style.backgroundColor = "#49CB8F";
+        inputNome.style.backgroundColor = "#FFFFFF";
+        botaoEditarNome.style.transition = "all 0.1s";
+    } else {
+        inputNome.setAttribute('readonly', 'readonly');
+        botaoEditarNome.innerHTML = "Editar nome";
+        botaoEditarNome.style.backgroundColor = "#7908e2";
+        inputNome.style.backgroundColor = "#D3D3D3";
+        await atualizarNome(idUsuario);
+    }
+}
+async function editarEmail(inputEmail, botaoEditarEmail, idUsuario) {
+    if (inputEmail.hasAttribute('readonly')) {
+        inputEmail.removeAttribute('readonly');
+        botaoEditarEmail.innerHTML = "Salvar";
+        botaoEditarEmail.style.backgroundColor = "#49CB8F";
+        inputEmail.style.backgroundColor = "#FFFFFF";
+        botaoEditarEmail.style.transition = "all 0.1s";
+    } else {
+        inputEmail.setAttribute('readonly', 'readonly');
+        botaoEditarEmail.innerHTML = "Editar email";
+        botaoEditarEmail.style.backgroundColor = "#7908e2";
+        inputEmail.style.backgroundColor = "#D3D3D3";
+        await atualizarEmail(idUsuario);
+    }
+}
+async function editarSenha(inputSenha, inputConfSenha, botaoSenha, idUsuario){
+    if (inputSenha.hasAttribute('readonly')) {
+        inputSenha.removeAttribute('readonly');
+        inputConfSenha.removeAttribute('readonly');
+        inputSenha.style.visibility= 'visible';
+        inputConfSenha.style.visibility= 'visible';
+        botaoSenha.innerHTML = "Salvar";
+        botaoSenha.style.backgroundColor = "#49CB8F";
+        inputSenha.style.backgroundColor = "#FFFFFF";
+        inputConfSenha.style.backgroundColor = "#FFFFFF";
+        botaoSenha.style.transition = "all 0.1s";
+    } else {
+        inputSenha.setAttribute('readonly', 'readonly');
+        inputConfSenha.setAttribute('readonly', 'readonly');
+        inputSenha.style.visibility='hidden';
+        inputConfSenha.style.visibility='hidden' 
+        botaoSenha.innerHTML = "Editar senha";
+        botaoSenha.style.backgroundColor = "#7908e2";
+        inputSenha.style.backgroundColor = "#D3D3D3";
+        inputConfSenha.style.backgroundColor = "#D3D3D3";
+        await atualizarSenha(idUsuario);
+    }
+}
+async function atualizarSenha(idUsuario){
+    inputSenha = document.getElementById("inputSenha");
+    inputConfSenha = document.getElementById("inputConfSenha");
+    if(inputSenha.value !== inputConfSenha.value){
+        alert("As senhas não são iguais!");
+    }
+    else if(inputSenha.value === "" || inputConfSenha.value === "") {
+        alert("Os campos não podem estar vazios");
+    }
+    else {
+        let senhaHash = await digestMessage(inputSenha.value);
+        const options = {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                id: idUsuario,
+                pw_hash: senhaHash
+            })
+        }
+        try{
+            const response = await fetch("http://localhost:3000/clientes/", options);
+            const usuarioJson = await response.json();
+            alert("Senha atualizada com sucesso");
+        }
+        catch(err){
+            console.log(err);
+            alert("Houve um erro");
+        }
+    }
+}
+
+async function atualizarNome(idUsuario){
+    let nomeUsuario = document.getElementById("inputNome").value
+    const options = {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            id: idUsuario,
+            nome: nomeUsuario
+        })
+      }
+    const response = await fetch("http://localhost:3000/clientes/", options);
+    const usuarioJson = await response.json();
+    inputNome.value = usuarioJson.nome;
+}
+
+async function atualizarEmail(idUsuario){
+    let emailUsuario = document.getElementById("inputEmail").value
+    const options = {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            id: idUsuario,
+            email: emailUsuario
+        })
+      }
+    const response = await fetch("http://localhost:3000/clientes/", options);
+    const usuarioJson = await response.json();
+    if(emailUsuario != usuarioJson.email) {
+        alert("Email já em uso");
+    }
+    inputEmail.value = usuarioJson.email;
+}
+async function receberInformacoes(idUsuario) {
+    const options = {
+        method: "GET",
+        headers: {
+          "Accept": "application/json",
+        }
+      }
+    try {
+        const response = await fetch("http://localhost:3000/clientes/?id="+idUsuario, options);
+        const usuarioJson = await response.json();
+        inputNome.value = usuarioJson.nome;
+        inputEmail.value = usuarioJson.email;
+    }
+    catch(err){
+        console.log(err);
+        alert("Houve um erro");
     }
 }
